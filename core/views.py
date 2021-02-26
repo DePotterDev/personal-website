7c3af2fed970baca7ff9fd78b0c8775cbc87763f
manalone <depotter.laurens@gmail.com>
2021-02-25 22:32:26 -0300

    Added GitHub GET REQUEST

--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import requests
 
 def home(request):
     """ Home Page """
-    return render(request, 'home.html')
+    response = requests.get('https://api.github.com/users/depotterdev/repos?per_page=5&sort=updated')
+    repo = response.json()
+    context = {'repo': repo, 'range': range(4)}
+    return render(request, 'home.html', context)
