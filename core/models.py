760002beef42e065040a36d6adf57e568600248f
manalone <depotter.laurens@gmail.com>
2021-03-02 16:25:41 -0300

    Adding models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+LANGUAGES_CHOICES = (
+    ('P', 'Python'),
+    ('D', 'Django'),
+    ('J', 'JavaScript'),
+    ('R', 'React.js'),
+    ('O', 'other'),
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    language = models.CharField(choices=LANGUAGES_CHOICES, max_length=50)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
