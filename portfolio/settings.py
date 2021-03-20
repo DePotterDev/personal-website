dff78c3194716ec28046da49afbd9c02663fb307
manalone <depotter.laurens@gmail.com>
2021-03-19 22:50:01 -0300

    Fixed settings .secrets

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-from ..secrets.secrets import *
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
