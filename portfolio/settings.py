e00e23a44913bc1420d182db98795517996d8937
manalone <depotter.laurens@gmail.com>
2021-03-23 02:05:29 -0300

    DEBUG FALSE

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['164.90.154.22', 'localhost', '127.0.0.1']
 
