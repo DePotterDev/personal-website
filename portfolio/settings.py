8dd0ed9928b98a5b2da570515e8912f152455a5f
manalone <depotter.laurens@gmail.com>
2021-03-22 23:58:52 -0300

    Fix settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -70,7 +70,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
